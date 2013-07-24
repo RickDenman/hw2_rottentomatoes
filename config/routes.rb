247c2d15d926658f75439fcb6d13a3c47599ed51
RickDenman <denman@southwestern.edu>
2013-07-24 05:51:33 -0700

    trying new route for root

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Rottenpotatoes::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'movie#index'
+  root :to => 'movies#home'
 
   # See how all your routes lay out with "rake routes"
 
